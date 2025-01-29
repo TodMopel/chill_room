1aa54bad4fcf653a5d87e3048ab0d6178384fc4b
TodMopel <benaitiervirgile@protonmail.com>
2025-01-29 15:19:57 +0100

    test

--- src/components/Room3D.js
+++ src/components/Room3D.js
@@ -32,43 +32,51 @@ const Room3D = ({ cameraConfig, onProjectSelect, disableRaycaster }) => {
   };
 
   return (
-    <Canvas
-      className="room-canvas"
-      shadows
-    >
+    <Canvas>
       <ambientLight intensity={0.5} />
-      <pointLight
-        position={[0, 4, 0]}
-        intensity={10}
-        castShadow={true}
-        shadow-mapSize-width={2048*2}
-        shadow-mapSize-height={2048*2}
-        shadow-radius={5}
-      />
-      <primitive
-        object={scene}
-        ref={roomRef}
-        castShadow={true}
-      />
-
-      <mesh position={[0, 1.5, 0]}>
-        <sphereGeometry args={[64, 32, 32]} />
-        
-        <meshStandardMaterial color="#444" side={THREE.BackSide} />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="red" />
       </mesh>
-
-      {!disableRaycaster && (
-        <RaycasterController 
-          handleObjectClick={handleClick} 
-          setHoveredObject={setHoveredObject}
-          hoveredObject={hoveredObject}
-        />
-      )}
-      {/* <FogEffect color="#fff" /> */}
-      <SelectedObjectEffect hoveredObject={hoveredObject} selectedObject={selectedObject} />
-      {cameraConfig && <CameraController cameraConfig={cameraConfig} roomRef={roomRef} />}
-      {window.location.hash === '#debug' && <Perf />}
     </Canvas>
+
+    // <Canvas
+    //   className="room-canvas"
+    //   shadows
+    // >
+    //   <ambientLight intensity={0.5} />
+    //   <pointLight
+    //     position={[0, 4, 0]}
+    //     intensity={10}
+    //     castShadow={true}
+    //     shadow-mapSize-width={2048*2}
+    //     shadow-mapSize-height={2048*2}
+    //     shadow-radius={5}
+    //   />
+    //   <primitive
+    //     object={scene}
+    //     ref={roomRef}
+    //     castShadow={true}
+    //   />
+
+    //   <mesh position={[0, 1.5, 0]}>
+    //     <sphereGeometry args={[64, 32, 32]} />
+        
+    //     <meshStandardMaterial color="#444" side={THREE.BackSide} />
+    //   </mesh>
+
+    //   {!disableRaycaster && (
+    //     <RaycasterController 
+    //       handleObjectClick={handleClick} 
+    //       setHoveredObject={setHoveredObject}
+    //       hoveredObject={hoveredObject}
+    //     />
+    //   )}
+    //   {/* <FogEffect color="#fff" /> */}
+    //   <SelectedObjectEffect hoveredObject={hoveredObject} selectedObject={selectedObject} />
+    //   {cameraConfig && <CameraController cameraConfig={cameraConfig} roomRef={roomRef} />}
+    //   {window.location.hash === '#debug' && <Perf />}
+    // </Canvas>
   );
 };
 
